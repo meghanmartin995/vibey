700a1829524c08d135ec7fc334dde5d413df8e66
Meghan Martin <meghanxmartin@gmail.com>
2020-05-21 22:46:58 +0200

    styles home

---
+++ app/javascript/packs/background.js
@@ -0,0 +1,13 @@
+// let angle = 0;
+
+// const header = document.querySelector('.home-header')
+
+// const background = function() {
+//   angle = angle + 1
+//   document.body.style.backgroundImage = "linear-gradient(30def, #ff4141, #2727e6)"
+
+// }
+
+// background()
+
+// export default background
