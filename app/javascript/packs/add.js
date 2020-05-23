a9c08626a07ca7a551d9dcf3e762dfa9030a2d4d
Meghan Martin <meghanxmartin@gmail.com>
2020-05-23 19:32:25 +0200

    adds toggle

---
+++ app/javascript/packs/add.js
@@ -0,0 +1,10 @@
+function add() {
+  const buttons = document.querySelector("buttons");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+export default add
