020859b6b3508c755de15e166a86ce5db9a81f4a
Meghan Martin <meghanxmartin@gmail.com>
2020-06-06 17:10:35 +0200

    adds captions

--- app/javascript/packs/websiteImage.js
+++ app/javascript/packs/websiteImage.js
@@ -1,21 +1,36 @@
+let image1 = {
+  imageFile: "https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706815/Black_Overlay_Fashion_Photo_Instagram_Post_21_umqdx9.png",
+  caption: "This is my caption"
+}
 
-const images = ["https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706815/Black_Overlay_Fashion_Photo_Instagram_Post_21_umqdx9.png", "https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706815/Black_Overlay_Fashion_Photo_Instagram_Post_21_umqdx9.png", "https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706814/Black_Overlay_Fashion_Photo_Instagram_Post_19_zhz1kq.png" ]
+let image2 = {
+  imageFile: "https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706815/Black_Overlay_Fashion_Photo_Instagram_Post_21_umqdx9.png",
+  caption: "This is my caption"
+}
+
+const images = [image1, image2]
 const section = document.querySelector(".website-images")
 const sectionLg = document.querySelector(".thirty-body")
-console.log(sectionLg)
+const header = document.querySelector(".thirty-header")
 
 let i = 0
 
 function websiteImage(x, y) {
-  const nextImage = images[i]
+  const nextImage = images[i].imageFile
+  const caption = images[i].caption
   const img = document.createElement("img")
+  const p = document.createElement("p")
+  p.innerHTML = caption
+
   img.setAttribute("src", nextImage)
   img.style.left = x + "px"
-  console.log (img.style.left)
   img.style.top = y + "px"
-  console.log (img.style.top)
   img.style.transform = "translate(-50%, -50%) scale(0.5) rotate(" + (Math.random() * 10 - 5) + "deg)"
+
+
   sectionLg.appendChild(img)
+  header.appendChild(p)
+  // node.after(img)
   i = i + 1
 
   if (i >= images.length) {
