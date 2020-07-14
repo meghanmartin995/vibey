f0a1e195b2e197d143f46fd54fd45b5f8ccde72d
Meghan Martin <meghanxmartin@gmail.com>
2020-07-14 17:01:55 +0200

    adds second modal

--- app/javascript/packs/home1.js
+++ app/javascript/packs/home1.js
@@ -17,24 +17,6 @@ function home1() {
         "src": `https://res.cloudinary.com/dq61mo2kj/image/upload/v1594661334/image_processing20200518-1504-r0dtrg-1_v2lygd.png`
     }, {
         'src': 'https://res.cloudinary.com/dq61mo2kj/image/upload/v1594670310/Fancier_Screens_v1-01_uavvpi.png'
-    }, {
-        'src': 'https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706808/Black_Overlay_Fashion_Photo_Instagram_Post_3_b0ovst.png'
-    }, {
-        'src': 'https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706814/Black_Overlay_Fashion_Photo_Instagram_Post_19_zhz1kq.png'
-    }, {
-        'src': 'https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706807/Black_Overlay_Fashion_Photo_Instagram_Post_6_aajuqj.png'
-    }, {
-        'src': 'https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706810/Black_Overlay_Fashion_Photo_Instagram_Post_17_exachq.png'
-    }, {
-        'src': 'https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706808/Black_Overlay_Fashion_Photo_Instagram_Post_9_vjudbc.png'
-    }, {
-        'src': 'https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706813/Black_Overlay_Fashion_Photo_Instagram_Post_16_a7emso.png'
-    }, {
-        'src': 'https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706814/Black_Overlay_Fashion_Photo_Instagram_Post_11_wgeyzb.png'
-    }, {
-        'src': 'https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706808/Black_Overlay_Fashion_Photo_Instagram_Post_15_gktbkj.png'
-    }, {
-        'src': 'https://res.cloudinary.com/dq61mo2kj/image/upload/v1590706816/Black_Overlay_Fashion_Photo_Instagram_Post_14_meaqtq.png'
     }
 
     ]
