e7295e41842bb85481d6a86e6b201e1e34abe8f1
Meghan Martin <meghanxmartin@gmail.com>
2020-07-14 15:20:57 +0200

    adds hover

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,6 +34,7 @@ import { toggle } from '../packs/toggle'
 import { home } from '../packs/home'
 import { home1 } from '../packs/home1'
 import { websiteImage } from '../packs/websiteImage'
+import { hover } from '../packs/hover'
 // import { flickity } from '../packs/flickity'
 
 document.addEventListener('turbolinks:load', () => {
---
+++ app/javascript/packs/hover.js
@@ -0,0 +1,34 @@
+const images = document.querySelectorAll(".project-home")
+
+const hover = () => {
+
+images.forEach(image => {
+  let currentX = 0
+  let currentY = 0
+  let aimX = 0
+  let aimY = 0
+
+  const updatePosition = function () {
+    currentX += (aimX - currentX) * 0.2
+    currentY += (aimY - currentY) * 0.2
+
+    const div = image.querySelector("img")
+
+    div.style.transform = `rotateX(${-18 * currentY}deg) rotateY(${18 * currentX}deg)`
+
+    requestAnimationFrame(updatePosition)
+  }
+
+  updatePosition()
+
+  document.addEventListener("mousemove", function (event) {
+    aimX = (event.pageX - (window.innerWidth / 2)) / window.innerWidth
+    aimY = (event.pageY - (window.innerHeight / 2)) / window.innerHeight
+  })
+})
+
+}
+
+hover()
+
+export default hover
